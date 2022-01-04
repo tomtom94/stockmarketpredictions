59213c61e317267c65869053336f295976b29d4c
Thomas Aumaitre <thomas.aumaitre@gmail.com>
2022-01-04 20:12:51 +0100

    heroku

---
+++ scripts/build.js
@@ -0,0 +1,23 @@
+const webpack = require("webpack");
+const rimraf = require("rimraf");
+const webpackConfig = require("../webpack/webpack.prod");
+
+const { compilerListener, paths, compilation } = require("./utils");
+
+const build = async () => {
+  try {
+    rimraf.sync(paths.build);
+
+    const compiler = webpack(webpackConfig);
+
+    compiler.run((err, stats) => compilation(err, stats, compiler.stats));
+
+    await compilerListener(compiler);
+
+    console.log("Webpack compilation client and server done !");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+build();
--- scripts/start.js
+++ scripts/start.js
@@ -1,10 +1,7 @@
-const webpack = require("webpack");
-const rimraf = require("rimraf");
-const webpackConfig = require("../webpack/webpack.prod");
 const express = require("express");
 const cors = require("cors");
 
-const { compilerListener, paths, compilation } = require("./utils");
+const { paths } = require("./utils");
 
 const PORT = process.env.PORT || 3030;
 
@@ -12,16 +9,6 @@ const app = express();
 
 const start = async () => {
   try {
-    rimraf.sync(paths.build);
-
-    const compiler = webpack(webpackConfig);
-
-    compiler.run((err, stats) => compilation(err, stats, compiler.stats));
-
-    await compilerListener(compiler);
-
-    console.log("Webpack compilation client and server done !");
-
     app.use(cors());
 
     app.use(express.static(paths.build));
